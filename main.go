8c16b32a981bc9738d43d80dc30fa207d829f1cf
AlexeyZXC <alexey.sakharov@list.ru>
2023-01-14 09:14:25 +0300

    martinit 2 chi

--- main.go
+++ main.go
@@ -4,9 +4,10 @@ import (
 	"elastic/handler"
 	"elastic/l"
 	"elastic/store"
+	"net/http"
 
-	"github.com/go-martini/martini"
-	"github.com/martini-contrib/render"
+	"github.com/go-chi/chi/v5"
+	render_chi "github.com/go-chi/render"
 )
 
 // Переписать не на Martini
@@ -19,16 +20,31 @@ func main() {
 	//Initialize Handlers
 	articleHandler := handler.NewArticleHandler(articleStore)
 	//Initialize Router
-	m := martini.Classic()
-	m.Use(render.Renderer())
-	//Routes
-	m.Get("/article/id/:id", articleHandler.Id)
-	m.Post("/article/add", articleHandler.Add)
-	m.Post("/article/search", articleHandler.Search)
+	// m := martini.Classic()
+	// m.Use(render.Renderer())
+	// //Routes
+	// m.Get("/article/id/:id", articleHandler.Id)
+	// m.Post("/article/add", articleHandler.Add)
+	// m.Post("/article/search", articleHandler.Search)
+	// panicHandler := handler.PanicHandler{}
+	// m.Get("/panic", panicHandler.Handle)
+	// m.Post("/log/add", panicHandler.Log)
+	// m.Run()
+
+	// chi
+	r := chi.NewRouter()
+	r.Use(render_chi.SetContentType(render_chi.ContentTypeJSON))
+
+	//routes
+	r.Get("Get", articleHandler.Id_chi)
+	r.Post("/article/add", articleHandler.Add_chi)
+	r.Post("/article/search", articleHandler.Search_chi)
+	//panic
 	panicHandler := handler.PanicHandler{}
-	m.Get("/panic", panicHandler.Handle)
-	m.Post("/log/add", panicHandler.Log)
-	m.Run()
+	r.Get("/panic", panicHandler.Handle_chi)
+	r.Post("/log/add", panicHandler.Log_chi)
+
+	http.ListenAndServe(":3333", r)
 }
 
 func parseErr(err error) {
